a044012a6055bb763604fde95b85642d5309aad2
l8k1b <abigail.theresa.moraes@gmail.com>
2023-08-28 20:44:01 -0700

    fixed import

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,7 @@
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import { SectionWrapper } from "../../hoc";
+import { SectionWrapper } from "../../hoc/";
 
 import CanvasLoader from "../Loader";
 
