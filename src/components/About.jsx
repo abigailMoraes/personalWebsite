dd193a5f36df36e2820b0b150bca354d71d47b61
l8k1b <abigail.theresa.moraes@gmail.com>
2023-08-28 20:28:39 -0700

    fixed import

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,10 @@
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-import Tilt from 'react-tilt';
-import { motion } from 'framer-motion';
-
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "./../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -18,19 +17,20 @@ const ServiceCard = ({ index, title, icon }) => {
           options={{
             max: 45,
             scale: 1,
-            speed: 450
+            speed: 450,
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly
        items-center flex-col"
         >
-          <img src={icon} alt={title}
-            className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
-  )
-}
+  );
+};
 
 const About = () => {
   return (
@@ -42,22 +42,22 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled computer science student with experience in TypeScript and JavaScript,
-        and expertise in frameworks like React, Node.js, and Three.js. I'm a quick
-        learner and I am passionate about creating efficient scalable, and
-        user-friendly solutions that solve real-world problems. Let's work together to build better products!
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled computer science student with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and I am passionate about creating
+        efficient scalable, and user-friendly solutions that solve real-world
+        problems. Let's work together to build better products!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
-          <ServiceCard key={service.title}
-            index={index} {...service} />
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default SectionWrapper(About, "about");
